45e6275cf98e612b8b721e241ee56975072c15ed
Takeru Suzuki <terkeljp@gmail.com>
2011-08-07 19:55:05 +0900

    added exception handling for detecting browser/engine versions

--- jquery.ua.js
+++ jquery.ua.js
@@ -25,13 +25,13 @@
     // detect browser
     b.name = (/(msie|firefox|chrome|safari|opera)/.exec(ua) || [, 'unknown'])[1]; // chrome must be tested before safari
     b[b.name] = true;
-    b.version = (b.unknown)? 0: /(?:msie |firefox\/|chrome\/|version\/)(\d+(\.\d+)*)/.exec(ua)[1];
+    b.version = (b.unknown)? 0: (/(?:msie |firefox\/|chrome\/|version\/)(\d+(\.\d+)*)/.exec(ua) || [, 0])[1];
     b.versionMajor = parseInt(b.version, 10);
 
     // detect engine
     e.name = (/(trident|webkit|gecko|presto)/.exec(ua) || [, 'unknown'])[1]; // webkit must be tested before gecko
     e[e.name] = true;
-    e.version = (e.unknown)? 0: /(?:trident\/|rv:|webkit\/|presto\/)(\d+(\.\d+)*)/.exec(ua)[1];
+    e.version = (e.unknown)? 0: (/(?:trident\/|rv:|webkit\/|presto\/)(\d+(\.\d+)*)/.exec(ua) || [, 0])[1];
     e.versionMajor = parseInt(e.version, 10);
 
     // add classes to html element
